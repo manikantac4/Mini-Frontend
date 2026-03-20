ac7f66cb3c3f890bc1f7a698fba666f1cc71bdac
Panduranga <panduranga1797@gmail.com>
2026-03-20 10:10:03 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ export default function App() {
     }, 4000)
 
     try {
-      const res  = await fetch('http://127.0.0.1:5000/detect-water', {
+      const res  = await fetch('https://mini-backend-lhd8.onrender.com/detect-water', {
         method:  'POST',
         headers: { 'Content-Type': 'application/json' },
         body:    JSON.stringify(params),
